67208be55ab73b6813bcdfe5a84215388c078376
limduhwan <limduhwan@gmail.com>
2020-02-26 08:46:41 +0900

    aaa

---
+++ ES16/splice.js
@@ -0,0 +1,10 @@
+var jbAry1 = [ 'One', 'Two', 'Three', 'Four', 'Five', 'Six' ];
+var jbSplice1 = jbAry1.splice( 2, 3 );
+console.log(jbSplice1);
+
+
+var jbAry2 = [ 'One', 'Two', 'Three', 'Four', 'Five', 'Six' ];
+var jbSplice2 = jbAry2.splice( 1, 2, 'Seven', 'Eight' );
+console.log(jbSplice2);
+console.log(jbAry2);
+
