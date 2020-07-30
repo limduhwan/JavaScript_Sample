ad98cafd9f9556a7ee69ef76d012b25c6a1f0404
limduhwan <limduhwan@gmail.com>
2020-07-31 08:01:37 +0900

    Revert "0730 test"
    This reverts commit d73a44ad03be0d299969f63494f7ec365bfaecd5.

--- ES16/match.js
+++
@@ -1,5 +0,0 @@
-const origin = 'https://vc1.c35p.com';
-const origin1 = 'http://localhost';
-console.log(origin.match(/(http|https):\/\/(vc1.c35p.com|localhost|192.168\.[\d]*\.[\d]*)(:[\d]*)*/g));
-
-console.log('test');
