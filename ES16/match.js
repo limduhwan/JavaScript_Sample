d73a44ad03be0d299969f63494f7ec365bfaecd5
limduhwan <limduhwan@gmail.com>
2020-07-31 08:01:05 +0900

    0730 test

---
+++ ES16/match.js
@@ -0,0 +1,5 @@
+const origin = 'https://vc1.c35p.com';
+const origin1 = 'http://localhost';
+console.log(origin.match(/(http|https):\/\/(vc1.c35p.com|localhost|192.168\.[\d]*\.[\d]*)(:[\d]*)*/g));
+
+console.log('test');
